7265decc6c89c28b9cfdedb60929b851ceb0b2c9
AkshataGopinath <47568382+AkshataGopinath@users.noreply.github.com>
2019-04-09 13:27:12 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Run handin template"
+
+# Script that pipes output of question 1 to a file
+echo "Run the first script ..."
+python3 qn1.py > qn1.txt
+
+# Script that pipes output of question 1 to a file
+echo "Run the second script ..."
+python3 qn2.py > qn2.txt
+
+# Script that pipes output of question 1 to a file
+echo "Run the third script ..."
+python3 qn3.py > qn3.txt
+
+echo "Generating the pdf"
+pdflatex report.tex
