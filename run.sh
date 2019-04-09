d6adf52335f9d4a1b7fd9710ed56a0af58f3dc8e
AkshataGopinath <47568382+AkshataGopinath@users.noreply.github.com>
2019-04-09 15:30:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,9 +10,5 @@ python3 qn1.py > qn1.txt
 echo "Run the second script ..."
 python3 qn2.py > qn2.txt
 
-# Script that pipes output of question 1 to a file
-echo "Run the third script ..."
-python3 qn3.py 
-
 echo "Generating the pdf"
 pdflatex report.tex
