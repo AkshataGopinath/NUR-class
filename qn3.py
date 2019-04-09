757de0b1d081627ee683770f56eed79f9188b492
AkshataGopinath <47568382+AkshataGopinath@users.noreply.github.com>
2019-04-09 14:17:54 +0200

    Add files via upload

--- qn3.py
+++ qn3.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 # ## part (a)
 
-satm11 = np.genfromtxt('satgals_m11.txt', dtype=float, skip_header=5)
 satm12 = np.genfromtxt('satgals_m12.txt', dtype=float, skip_header=5)
 satm13 = np.genfromtxt('satgals_m13.txt', dtype=float, skip_header=5)
 satm14 = np.genfromtxt('satgals_m14.txt', dtype=float, skip_header=5)
